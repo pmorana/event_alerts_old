e92162fede4f39a9bc2a6735b246b10510889f93
Prelang Builder <builder@prelang.com>
2015-12-10 19:36:47 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Event' to allow for create, update

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Event do
+  permit_params :title, :info, :event_start, :event_end
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
